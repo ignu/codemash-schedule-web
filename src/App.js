5197feb8e7e2ee9224f46ca440fa7b2ff9cbb9d1
Len Smith <len@barrison.com>
2017-06-11 02:09:55 -0700

    Display Sessions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import R from "ramda";
+import "./App.css";
+
+import sessions from "./data/sessions.js";
+
+const Session = props => {
+  const { Title, Abstract, Category, Tags } = props;
+
+  return (
+    <li>
+      <div>{Title}</div>
+      <div>{Abstract}</div>
+      <div>{Category}</div>
+      <div>{Tags.join(',')}</div>
+    </li>
+  );
+};
+
+const SessionList = props => {
+  return (<div>
+    <ul>
+     { R.map(s => <Session key={s.Id} {...s} />, sessions) }
+    </ul>
+  </div>)
+};
 
 class App extends Component {
   render() {
@@ -10,9 +34,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SessionList />
       </div>
     );
   }
